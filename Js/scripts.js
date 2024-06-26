c42b772d90d58946623711f4bae50d511ee7a326
samdoble <148961297+samdoble@users.noreply.github.com>
2024-06-26 22:53:42 +1000

    Add files via upload

---
+++ Js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+// Example of a simple alert function on the homepage
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (document.querySelector('body').classList.contains('home')) {
+        alert("Welcome to Chicken Wings Paradise!");
+    }
+});
